8e4301f6ed320ca37f71e84e2039ec26f1e620c9
blessed-mouse-7746 <nyabutijoash@yahoo.com>
2019-05-07 11:30:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Htmldoctor"
-var height = 6
+var height = "6"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height+1)
+console.log(parseInt(height)+1)
